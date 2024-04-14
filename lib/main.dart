ef56c484de32f43362975c22c1eb0f49f9b12e61
LFPires1993 <lfpires1993@gmail.com>
2024-04-14 17:44:25 -0300

    Finalize app

--- lib/main.dart
+++ lib/main.dart
@@ -24,101 +24,161 @@ class _AppHomeState extends State<AppHome> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(
-            onPressed: clearFields,
-            icon: const Icon(
-              Icons.refresh_sharp,
-              size: 27,
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              onPressed: clearFields,
+              icon: const Icon(
+                Icons.refresh_sharp,
+                size: 27,
+              ),
+              color: Colors.white,
+            )
+          ],
+          title: const Text(
+            'Calculadora de IMC',
+            style: TextStyle(
+              color: Colors.white,
             ),
-            color: Colors.white,
-          )
-        ],
-        title: const Text(
-          'Calculadora de IMC',
-          style: TextStyle(
-            color: Colors.white,
           ),
+          centerTitle: true,
+          backgroundColor: Colors.green,
         ),
-        centerTitle: true,
-        backgroundColor: Colors.green,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 16),
-        child: Center(
-          child: Column(
-            children: [
-              const Icon(Icons.person_outlined, size: 200, color: Colors.green),
-              TextField(
-                textAlign: TextAlign.center,
-                keyboardType: TextInputType.number,
-                controller: _weightController,
-                decoration: InputDecoration(
-                  errorText: _weightError,
-                  labelText: 'Peso (kg)',
-                  labelStyle:
-                      const TextStyle(fontSize: 20, color: Colors.green),
-                  enabledBorder: const UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.green)),
-                  focusedBorder: const UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.green)),
-                ),
-              ),
-              const SizedBox(height: 20),
-              const Icon(Icons.add, size: 23, color: Colors.green),
-              TextField(
-                textAlign: TextAlign.center,
-                keyboardType: TextInputType.number,
-                controller: _heightController,
-                decoration: InputDecoration(
-                  errorText: _heightError,
-                  labelText: 'Altura (cm)',
-                  labelStyle:
-                      const TextStyle(fontSize: 20, color: Colors.green),
-                  enabledBorder: const UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.green)),
-                  focusedBorder: const UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.green)),
-                ),
-              ),
-              const SizedBox(height: 20),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.green,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 16),
+            child: Center(
+              child: Column(
+                children: [
+                  const Icon(Icons.person_outlined,
+                      size: 200, color: Colors.green),
+                  TextField(
+                    textAlign: TextAlign.center,
+                    keyboardType: TextInputType.number,
+                    controller: _weightController,
+                    style: const TextStyle(color: Colors.green, fontSize: 25),
+                    decoration: InputDecoration(
+                      errorText: _weightError,
+                      labelText: 'Peso (kg)',
+                      labelStyle:
+                          const TextStyle(fontSize: 20, color: Colors.green),
+                      enabledBorder: const UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.green)),
+                      focusedBorder: const UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.green)),
                     ),
                   ),
-                  child: const Text(
-                    'Calcular',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
+                  const SizedBox(height: 20),
+                  const Icon(Icons.add, size: 23, color: Colors.green),
+                  TextField(
+                    textAlign: TextAlign.center,
+                    keyboardType: TextInputType.number,
+                    controller: _heightController,
+                    decoration: InputDecoration(
+                      errorText: _heightError,
+                      labelText: 'Altura (cm)',
+                      labelStyle:
+                          const TextStyle(fontSize: 20, color: Colors.green),
+                      enabledBorder: const UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.green)),
+                      focusedBorder: const UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.green)),
                     ),
                   ),
-                ),
+                  const SizedBox(height: 20),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: calculateIMC,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                      ),
+                      child: const Text(
+                        'Calcular',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                  Text(
+                    _result,
+                    style: const TextStyle(
+                      color: Colors.green,
+                      fontSize: 25,
+                    ),
+                  ),
+                ],
               ),
-              const SizedBox(height: 12),
-              Text(_result, style: const TextStyle(
-                color: Colors.green,
-                fontSize: 25,
-              ),),
-            ],
+            ),
           ),
         ),
       ),
     );
   }
 
+  calculateIMC() {
+    int? height = int.tryParse(_heightController.text);
+    if (height == null) {
+      setState(() {
+        _heightError = 'Altura é um campo obrigatório!';
+        _weightError = null;
+      });
+      return;
+    }
+
+    int? weight = int.tryParse(_weightController.text);
+    if (weight == null) {
+      setState(() {
+        _weightError = 'Peso é um campo obrigatório!';
+        _heightError = null;
+      });
+      return;
+    }
+
+    final double imc = weight / (height * 2) * 100;
+
+    setResult(imc);
+  }
+
   clearFields() {
     setState(() {
       _heightController.text = '';
       _weightController.text = '';
+    });
+  }
+
+  setResult(double imc) {
+    final String result;
+    if (imc < 16) {
+      result = 'IMC: ${imc.toStringAsFixed(2)} = Magreza grave!';
+    } else if (imc < 17) {
+      result = 'IMC: ${imc.toStringAsFixed(2)} = Magreza moderada!';
+    } else if (imc < 18.6) {
+      result = 'IMC: ${imc.toStringAsFixed(2)} = Magreza leve!';
+    } else if (imc < 25) {
+      result = 'IMC: ${imc.toStringAsFixed(2)} = Peso ideal!';
+    } else if (imc < 30) {
+      result = 'IMC: ${imc.toStringAsFixed(2)} = Sobrepeso!';
+    } else if (imc < 35) {
+      result = 'IMC: ${imc.toStringAsFixed(2)} = Obesidade grau 1!';
+    } else if (imc < 40) {
+      result = 'IMC: ${imc.toStringAsFixed(2)} = Obesidade grau 2!';
+    } else {
+      result = 'IMC: ${imc.toStringAsFixed(2)} = Obesidade grau 3!';
+    }
+
+    setState(() {
+      _weightError = null;
+      _heightError = null;
+      _result = result;
     });
   }
 }
